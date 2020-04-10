94cac2b203c90e17017536c50aee882b9201fd06
George Alan <georgealanrufo@gmail.com>
2020-04-09 22:58:25 -0300

    continuando os estudos.

--- RestAssured/src/test/java/com/restassured/tests/TestJson.kt
+++ RestAssured/src/test/java/com/restassured/tests/TestJson.kt
@@ -7,6 +7,10 @@ import io.restassured.response.Response
 import org.hamcrest.Matchers
 import org.junit.Assert
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+import java.math.MathContext
+import java.util.*
+import kotlin.collections.ArrayList
 
 class TestJson {
 
@@ -76,9 +80,60 @@ class TestJson {
     fun `deve verificar lista na raiz`() {
         RestAssured.given()
                 .`when`()
-                .get("https://jsonplaceholder.typicode.com/users/1/posts")
+                .get("https://restapi.wcaquino.me/users")
                 .then()
                 .statusCode(200)
-                .body("$", Matchers.hasSize<Int>(10)) // O $ é só uma convenção pode usar "" vazio que funciona.
+                .body("$", Matchers.hasSize<Int>(3)) // O $ é só uma convenção pode usar "" vazio que funciona.
+                .body("name", Matchers.hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+                .body("age[1]", Matchers.`is`(25))
+                .body("filhos.name", Matchers.hasItem(Arrays.asList("Zezinho", "Luizinho")))
+                .body("salary", Matchers.contains(1234.5678f, 2500, null))
+    }
+
+    @Test
+    fun `deve fazer verificacoes avancadas`() {
+        RestAssured.given()
+                .`when`()
+                .get("https://restapi.wcaquino.me/users")
+                .then()
+                .statusCode(200)
+                .body("$", Matchers.hasSize<Int>(3))
+                .body("age.findAll{it <= 25}.size()", Matchers.`is`(2))
+                .body("age.findAll{it <= 25 && it > 20}.size()", Matchers.`is`(1))
+                .body("findAll{it.age <= 25 && it.age > 20}.name", Matchers.hasItem("Maria Joaquina"))
+                .body("findAll{it.age <= 25}[0].name", Matchers.`is`("Maria Joaquina"))
+                .body("findAll{it.age <= 25}[-1].name", Matchers.`is`("Ana Júlia"))
+                .body("find{it.age <= 25}.name", Matchers.`is`("Maria Joaquina"))
+                .body("findAll{it.name.contains('n')}.name", Matchers.hasItems("Maria Joaquina", "Ana Júlia"))
+                .body("findAll{it.name.length() > 10}.name", Matchers.hasItems("João da Silva", "Maria Joaquina"))
+                .body("name.collect{it.toUpperCase()}", Matchers.hasItem("MARIA JOAQUINA"))
+                .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", Matchers.hasItem("MARIA JOAQUINA"))
+                .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", Matchers.allOf(Matchers.arrayContaining("MARIA JOAQUINA"), Matchers.arrayWithSize(1)))
+                .body("age.collect{it * 2}", Matchers.hasItems(60, 50, 40))
+                .body("id.max()", Matchers.`is`(3))
+                .body("salary.min()", Matchers.`is`(1234.5678f))
+                .body("salary.findAll{it != null}.sum()", Matchers.allOf(Matchers.greaterThan(3000.0), Matchers.lessThan(5000.0)))
+                //.body("salary.findAll{it != null}.sum()", Matchers.`is`(Matchers.closeTo(3734.5678f, 0.001))) // Problemas com conversão de tipo Java.
+    }
+
+    /**
+     * Podemos unir o poder das consiltas avançadas com o JsonPath para facilitar, pois
+     * unindo podemos quebrar o retorno em um tipo de objeto/atributo para poder utilizar
+     * com o Assert do Junit de forma mais simples.
+     * Abaixo quebramos uma consulta e tipamos o retorno como um ArrayList(String) e com isso podemos trabalhar
+     * em cima desse atributo para comparação.
+     */
+    @Test
+    fun `unindo jsonPath com Kotlin`() {
+        val names: ArrayList<String> = RestAssured.given()
+                .`when`()
+                .get("https://restapi.wcaquino.me/users")
+                .then()
+                .statusCode(200)
+                .extract().path("name.findAll{it.startsWith('Maria')}")
+
+        Assert.assertEquals(1, names.size)
+        Assert.assertTrue(names.get(0).equals("Maria Joaquina"))
+        Assert.assertEquals(names.get(0).toUpperCase(), "maria joaquina".toUpperCase())
     }
 }
